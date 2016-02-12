449a368844038136489b96d8e525a73024412037
Nash <sawmebabe@gmail.com>
2016-02-12 13:26:58 +0200

    Minor Edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  use_doorkeeper
-  devise_for :users
   root 'home#index'
   get 'home/index'
+
+  use_doorkeeper
+  devise_for :users
 
   get '/me' => "application#me"
 
